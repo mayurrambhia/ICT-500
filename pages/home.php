64727854b3991e48485b46008aa8d9765aac9894
mayurrambhia <mayur.rambhia@gmail.com>
2016-02-11 00:48:18 -0500

    UI Changes
    Some changes to directory structure and created a home page

---
+++ pages/home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+    <title>GitHub Tutorial</title>
+    <script src="http://code.jquery.com/jquery-1.8.2.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="../css/base-admin-2.css" rel="stylesheet">
+    <link type="text/css" href="../css/bootstrap.css" rel="stylesheet" />
+    <link type="text/css" href="../css/bootstrap.min.css" rel="stylesheet" />
+    <link type="text/css" href="../css/bootstrap-responsive.css" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <script type="text/javascript" src="../js/bootstrap.js"></script>
+    <script src="../js/genJS.js"></script>
+    <link type="text/css" href="../css/style.css" rel="stylesheet" />
+
+</head>
+    <body>
+        <!-- BreadCrumbs -->
+        <div class="row">
+            <span class="col-md-1"></span>
+            <span class="col-md-10">
+                <ol class="breadcrumb">
+                    <li><a href="active">Home</a></li>
+                   
+                </ol>
+            </span>
+            <span class="col-md-1"></span>
+        </div>
+        <!-- Head -->
+        <div class="row">
+            <span class="col-md-1"></span>
+            <span class="col-md-10">
+                <div id="header">
+                    <h1> GIT Tutorial </h1>
+                </div>
+            </span>
+            <span class="col-md-1"></span>
+        </div>
+        
+        
+        <!-- Tabs -->
+        <div class="row">
+            <span class="col-md-1"></span>
+            <span class="col-md-10">
+                <ul class="nav nav-pills">
+                      <li role="presentation" class="active"><a href="#">Home</a></li>
+                      <li role="presentation"><a href="#">Profile</a></li>
+                      <li role="presentation"><a href="#">Messages</a></li>
+                </ul>
+            </span>
+            <span class="col-md-1"></span>
+        
+        <div class="row">
+            <span class="col-md-2"></span>
+            <span class="col-md-8">
+                <div id="main">
+                    GitHub is an open-source web based repository system founded by <strong>Tom Preston-Werner, Chris Wanstrath</strong> and <strong>PJ Hyett</strong>.<br>
+                    <ul>
+                        <li>It provides a web based graphical interface with desktop and mobile device integration.</li>
+                        <li>Along with Source Code Management, it provides several collaboration features like: 
+                            <ul>
+                                <li> Bug Tracking</li>
+                                <li> Feature Requests</li>
+                                <li> Task Management</li>
+                            </ul>
+                        </li>
+                    </ul>
+                    Here, you can find a brief tutorial in using GitHub to ease the management of your source code.<br>
+                    
+                    <a href="#"><button type="button"> Begin Tutorial !</button></a>
+                </div>
+            </span>
+            <span class="col-md-2"></span>
+        </div>
+    </body>
+</html>
